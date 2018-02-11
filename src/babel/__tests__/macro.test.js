c4abbb968227f4366a09fab0af6fd61c23e24abf
Moti Zilberman <motiz88@gmail.com>
2018-02-11 14:32:48 +0000

    test: create explicit test for runtime fallback from macro

--- src/babel/__tests__/macro.test.js
+++ src/babel/__tests__/macro.test.js
@@ -1,5 +1,14 @@
+import { Animated } from "react-native";
+import macro from "../macro";
+const { Node: AnimatedNode } = Animated;
 import pluginTester from "babel-plugin-tester";
 import plugin from "babel-plugin-macros";
+
+describe("macro", () => {
+  test("falls back to runtime", () => {
+    expect(macro`1`).toBeInstanceOf(AnimatedNode);
+  });
+});
 
 pluginTester({
   plugin,
